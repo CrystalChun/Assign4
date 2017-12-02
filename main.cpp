3dfd9e7502cfbdf7166e0fdc37ec42716b4f954e
Crystal Chun <crystalchun@macbook-air.lan>
2017-12-02 14:27:43 -0800

    Updated main

--- main.cpp
+++ main.cpp
@@ -7,51 +7,32 @@
 #include <string>
 #include "semaphore.h"
 using namespace std;
-enum{PROC1, PROC2, PROC3, PROC4};
+
+void childProc(SEMAPHORE &, int);
+void quit(SEMAPHORE &, pid_t []);
 /*
 Problems:
-1. How do we determine which integer the child will work on?
-2. How do we spawn more than one child at a time?
-3. Queueing the children and activating the next one?
+1. Determining whether to work on u or v. Idea: shared memory, put in whichever is available. 
 */
 int main(int argc, const char * argv[]) {
     const int u = 827395609;
     const int v = 962094883;
     int children = 0;
-    int max = 0;
-    SEMAPHORE sem(1);
-    sem.V(max);
-    sem.V(max);
-    pid_t childrenPid[4];
+    int execute = 0;
     pid_t pid;
+    pid_t childrenPid[4];
+    SEMAPHORE sem(1);
+
+    sem.V(execute);
+    sem.V(execute);
+    
     // Spawn 4 children, only two at a time and cannot work on same int
     
     while(children < 4) { // Spawns only 4 children... hopefully
         if((pid = fork()) == 0) {
             cout << "In child " << getpid() << endl;
             // Child, loop in here and never break out
-            int x = u;
-            bool resume = true;
-            while(true) {
-                sem.P(max);
-                if(resume) {
-                    cout << getpid() << " running" << endl;
-                    resume = false;
-                }
-                while(true) {
-                    int randNum = rand(); // Generate random numbers
-                    // Test if number less than 100 or divisible by X
-                    if(randNum < 100 || randNum % x == 0) {
-                        // Break out and queue itself
-                        cout << getpid() << " Leaving, " << randNum << endl;
-                        sem.V(max);
-                        if(!resume) {
-                            resume = true;
-                        }
-                        break;
-                    }
-                }
-            }
+            childProc(sem, execute);
         } else {
             cout << "Recording child # " << children << " pid: " << pid << endl;
             childrenPid[children] = pid;
@@ -60,20 +41,58 @@ int main(int argc, const char * argv[]) {
         }
     }
     cout << getpid() << " Parent finished making children" << endl;
+
     // After spawning 4 children, parent comes out here 
-    while(true) {
-        string sig;
-        // Listen for !wq
-        while(getline(cin, sig)) {
-            if(sig == "!wq") break;
-        }
-        // Got !wq and aborts children
-        for(pid_t pid : childrenPid) {
-            cout << "Killing: " << pid << endl;
-            kill(pid, SIGTERM);
-        }
-        break;
+
+    string sig;
+
+    // Listen for !wq
+    while(getline(cin, sig)) {
+        if(sig == "!wq") break;
     }
+
+    // Got !wq and aborts children
+    quit(sem, childrenPid);
+}
+
+void childProc(SEMAPHORE & sem, int execute) {
+    int x = u;
+    bool resume = true;
+    while(true) {
+        sem.P(execute);
+        // Read queue for number and assign here
+        if(resume) {
+            cout << getpid() << " running" << endl;
+            resume = false;
+        }
+        while(true) {
+            int randNum = rand(); // Generate random numbers
+            // Test if number less than 100 or divisible by X
+            if(randNum < 100 || randNum % x == 0) {
+                // Break out and queue itself
+                cout << getpid() << " Leaving, " << randNum << endl;
+                sem.V(execute);
+                if(!resume) {
+                    resume = true;
+                }
+                // Put number in queue
+                break;
+            }
+        }
+    }
+}
+/**
+
+*/
+void quit(SEMAPHORE & sem, pid_t children) {
+    // Goes through all children and kills them.
+    for(pid_t pid : children) {
+        cout << "Killing: " << pid << endl;
+        kill(pid, SIGTERM);
+    }
+
     cout << "Killed all children, parent leaving" << endl;
+
+    // Removes semaphore
     sem.remove();
 }
