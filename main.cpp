cb7dcf9a413c7da8e8d6b868acbdab22724da486
Crystal Chun <crystalchun@MacBook-Air.local>
2017-12-03 15:32:50 -0800

    Updated to use shared mem

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ void childProc(SEMAPHORE & sem, char * shmbuf) {
                 sem.P(BUF);
                 *(shmbuf + index) = '1';
                 sem.V(BUF);
-                sem.V(execute);
+                sem.V(EXE);
                 if(!resume) {
                     resume = true;
                 }
