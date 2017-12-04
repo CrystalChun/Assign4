6060ac674bc674f3aa80262249d2e912a6a188cc
Crystal Chun <crystalchun@MacBook-Air.local>
2017-12-03 16:20:36 -0800

    Updated to use shared mem

--- main.cpp
+++ main.cpp
@@ -11,114 +11,131 @@
 #include <string>
 #include "semaphore.h"
 using namespace std;
-enum {EXE, BUF};
+enum {EXE, BUF}; // Semaphore ids, EXE is for executing a child process, BUF is for entering the critical area (aka shared memory)
 void childProc(SEMAPHORE &, char*);
 void quit(SEMAPHORE &, pid_t [], int);
-/*
-Problems:
-1. Determining whether to work on u or v. Idea: shared memory, put in whichever is available. 
-*/
-int main(int argc, const char * argv[]) {
-    const int BUFFSIZE = 2;
 
+int main(int argc, const char * argv[]) {
+    const int BUFFSIZE = 2; // Shared memory will only have two characters in it
     int children = 0;
 
+    // Keeps track of the process IDs of all the children
     pid_t pid;
     pid_t childrenPid[4];
+
+    // Initialize shared memory
+	int shmid = shmget(IPC_PRIVATE, BUFFSIZE * sizeof(char), PERMS); // allocated shared memory
+	char * shmBUF = (char *)shmat(shmid, 0, SHM_RND); // attaching shared memory
+
+    // Initialize semaphores
     SEMAPHORE sem(2);
-
-    // IPC_PRIVATE is replacement for ftok
-	int shmid = shmget(IPC_PRIVATE, BUFFSIZE * sizeof(char), PERMS); // allocated shared memory (not attached yet)
-	char * shmBUF = (char *)shmat(shmid, 0, SHM_RND); // attaching to that shared memory - pointer that points to shared memor
-
-    sem.V(EXE);
-    sem.V(EXE);
-    sem.V(BUF);
-
-    *(shmBUF + 0) = '1';
-    *(shmBUF + 1) = '1';
-    cout << "u: " << *(shmBUF + 0) << endl << "v: " << *(shmBUF + 1) << endl;
-    // Spawn 4 children, only two at a time and cannot work on same int
     
-    while(children < 4) { // Spawns only 4 children... hopefully
-        if((pid = fork()) == 0) {
+    sem.V(EXE); 
+    sem.V(EXE); // EXE = 2
+    sem.V(BUF); // BUF = 1
+
+    // Setup flags in shared memory, 1 = available, 0 = not available
+    *(shmBUF + 0) = '1'; // Flag that u is available
+    *(shmBUF + 1) = '1'; // Flag that v is available
+
+    // Spawning 4 children
+    while(children < 4) { 
+        if((pid = fork()) == 0) { // In child
             cout << "In child " << getpid() << endl;
-            // Child, loop in here and never break out
+            // Child process
             childProc(sem, shmBUF);
         } else {
             cout << "Recording child # " << children << " pid: " << pid << endl;
+            // Adding child's process ID into list
             childrenPid[children] = pid;
-            // Parent - listens for !wq, when it gets it, abort all children
             children ++;
         }
     }
     cout << getpid() << " Parent finished making children" << endl;
 
-    // After spawning 4 children, parent comes out here 
 
-    string sig;
+    string endSignal;
 
-    // Listen for !wq
-    while(getline(cin, sig)) {
-        if(sig == "!wq") break;
+    // Listens for "!wq"
+    while(getline(cin, endSignal)) {
+        if(endSignal == "!wq") break;
     }
 
     // Got !wq and aborts children
     quit(sem, childrenPid, shmid);
 }
 
+int getModNum(char * shmbuf) {
+    // Choosing the mod number
+    if(*(shmbuf + 0) == '1') { // Using u
+        // Flag to prevent use of integer u
+        *(shmbuf + 0) = '0'; 
+        return 0;
+    } else if (*(shmbuf + 1) == '1') { // Using v
+        // Flag to prevent use of v integer
+        *(shmbuf + 1) = '0';
+        return 1;
+    } 
+}
 void childProc(SEMAPHORE & sem, char * shmbuf) {
-    bool resume = true;
-    int index = 0;
+// Get rid when turning in
+    bool resume = true; 
+//
+    int index = 0; // Index of shared memory this process is using, 0 = u, 1 = v
+
     while(true) {
         int modNum = 1;
         sem.P(EXE);
-        sem.P(BUF);
-        if(*(shmbuf + 0) == '1') {
-            // Using u
-            modNum = 827395609;
-            *(shmbuf + 0) = '0';
-            index = 0;
-        } else if (*(shmbuf + 1) == '1') {
-            // Using v
-            modNum = 962094883;
-            *(shmbuf + 1) = '0';
-            index = 1;
-        }
-        sem.V(BUF);
 
+        // Accessing critical area (shared memory)
+        sem.P(BUF); 
+        index = getModNum(shmbuf);
+        sem.V(BUF);
+// Get rid
         if(resume) {
             cout << "NEW CHILD: " << getpid() << " running, using: " << modNum << endl;
             resume = false;
         }
+//
+        modNum = index == 0 ? 827395609 : 962094883;
+        // Generates random numbers until number is less than 100 or divisible by its modNum
         while(true) {
             int randNum = rand(); // Generate random numbers
+
             // Test if number less than 100 or divisible by X
             if(randNum < 100 || randNum % modNum == 0) {
-                // Break out and queue itself
+                // Breaks out of loop (no longer running)
                 cout << getpid() << " Leaving: " << randNum << " index: " << index<< endl;
+                
+                // Update flag in critical area to allow next process to use this modNum
                 sem.P(BUF);
                 *(shmbuf + index) = '1';
-                sem.V(BUF);
-                sem.V(EXE);
+                sem.V(BUF); 
+
+                sem.V(EXE); // Resumes next process in queue
+// Get rid
                 if(!resume) {
                     resume = true;
                 }
+//
                 break;
             }
         }
     }
 }
 /**
-
+* Cleans up the program by killing all 4 children process 
+* and removing both the shared memory and the semaphores.
+* 
 */
 void quit(SEMAPHORE & sem, pid_t children[], int shmid) {
-    // Goes through all children and kills them.
-    for(int i = 0; i < sizeof(*children); i++) {
+    // Goes through all children process IDs and kills them.
+    for(int i = 0; i < sizeof(* children); i++) {
         cout << "Killing: " << children[i] << endl;
         kill(children[i], SIGTERM);
     }
-    shmctl(shmid, IPC_RMID, NULL);	/* cleaning up */
+    // Cleans up shared memory
+    shmctl(shmid, IPC_RMID, NULL);
     cout << "Killed all children, parent leaving" << endl;
 
     // Removes semaphore
