70cb76645bdcfefb51eebbe5e7ba3195d34b7b48
Crystal Chun <crystalchun@MacBook-Air.local>
2017-12-10 15:06:52 -0800

    Added comments and documentation

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/**
+    Crystal Chun 012680952
+    File names associated with this assignment: 
+        main.cpp, semaphore.cpp, semaphore.h
+    Description:
+        A parent process generates four child processes.
+        Only two child processes can run at a given time.
+        Each child works on a specified integer
+        (u = 827395609 or v = 962094883), but they cannot
+        work on the same integer. A child will continuously 
+        generate random numbers until it hits the stopping 
+        condition, which is when the number generated is less than
+        100 or a factor of the integer the child is work on 
+        (either u or v). When the child hits the stopping condition,
+        it will surrender its turn at the cpu, add itself to the queue, and allow
+        the next child to run.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -10,14 +28,24 @@
 #include <iostream>
 #include <string>
 #include "semaphore.h"
+
 using namespace std;
-enum {EXE, BUF}; // Semaphore ids, EXE is for executing a child process, BUF is for entering the critical area (aka shared memory)
-int getModNum(char * );
-void childProc(SEMAPHORE &, char*);
+
+// Semaphore IDs: 
+// EXE is for executing a child process
+// BUF is for entering critical area (shared memory)
+enum {EXE, BUF};
+
+// Function headers
+int getModNum(char *);
+void childProc(SEMAPHORE &, char *);
 void quit(SEMAPHORE &, pid_t [], int);
 
 int main(int argc, const char * argv[]) {
-    const int BUFFSIZE = 2; // Shared memory will only have two characters in it
+    // Maximum size of shared memory = 2 characters
+    const int BUFFSIZE = 2; 
+
+    // Keeps track of the number of children created
     int children = 0;
 
     // Keeps track of the process IDs of all the children
@@ -27,116 +55,128 @@ int main(int argc, const char * argv[]) {
     // Initialize semaphores
     SEMAPHORE sem(2);
     
+    // Initialize EXE semaphore to 2
     sem.V(EXE); 
-    sem.V(EXE); // EXE = 2
-    sem.V(BUF); // BUF = 1
+    sem.V(EXE); 
+
+    // Initialize BUF semaphore to 1
+    sem.V(BUF); 
 
     // Initialize shared memory
-	int shmid = shmget(IPC_PRIVATE, BUFFSIZE * sizeof(char), PERMS); // allocated shared memory
-	char * shmBUF = (char *)shmat(shmid, 0, SHM_RND); // attaching shared memory
+	int shmid = shmget(IPC_PRIVATE, BUFFSIZE * sizeof(char), PERMS); // allocated shared memory (2 characters)
+	char * shmBUF = (char *) shmat(shmid, 0, SHM_RND); // attaching shared memory
 
-    // Setup flags in shared memory, 1 = available, 0 = not available
+    // Setup flags in shared memory, '1' = available, '0' = not available
     *(shmBUF + 0) = '1'; // Flag that u is available
     *(shmBUF + 1) = '1'; // Flag that v is available
 
     // Spawning 4 children
     while(children < 4) { 
         if((pid = fork()) == 0) { // In child
-            cout << "In child " << getpid() << endl;
             childProc(sem, shmBUF);
         } else { // In parent
-            cout << "Recording child # " << children << " pid: " << pid << endl;
             // Adding child's process ID into list
             childrenPid[children] = pid;
             children ++;
         }
     }
-    cout << getpid() << " Parent finished making children" << endl;
 
+    // Initialize string to listen for end signal
     string endSignal;
 
     // Listens for "!wq"
     while(getline(cin, endSignal)) {
-        if(endSignal == "!wq") break;
+        if(endSignal == "!wq") break; // When we get "!wq" we break out of loop
     }
 
-    // Got !wq and aborts children
+    // Got !wq so aborts children and exits
     quit(sem, childrenPid, shmid);
 }
 
+/**
+Gets the mod number (u or v) for the child process by checking the shared memory. 
+The mod number must not be used by another child process.
+    @param shmbuf - The shared memory used to determine which integer to use.
+    @return An index (0 for u, 1 for v) that determines which integer is free
+        to be used by the child.
+*/
 int getModNum(char * shmbuf) {
-    // Choosing the mod number
-    if(*(shmbuf + 0) == '1') { // Using u
+    // Checks shared memory to see which integer is not being in use
+    if(*(shmbuf + 0) == '1') { // Integer u is not in use, use u.
         // Flag to prevent use of integer u
         *(shmbuf + 0) = '0'; 
         return 0;
-    } else if (*(shmbuf + 1) == '1') { // Using v
+    } else if (*(shmbuf + 1) == '1') { // Integer v is not in use, use v.
         // Flag to prevent use of v integer
         *(shmbuf + 1) = '0';
         return 1;
     } 
 }
+
+/**
+Where a child process lives. The child process will
+continually generate random numbers until the number is either
+less than 100 or the number is a factor of the number the 
+child process is assigned to work on. 
+    @param sem - The semaphore object.
+    @param shmbuf - The shared memory object.
+*/
 void childProc(SEMAPHORE & sem, char * shmbuf) {
-// Get rid when turning in
-    bool resume = true; 
-//
-    int index = 0; // Index of shared memory this process is using, 0 = u, 1 = v
+    // Initialize index of shared memory this process will use
+    int index = 0; // 0 = u, 1 = v
 
     while(true) {
+        // Initialize the mod number
         int modNum = 1;
+
+        // Queue itself if at least two child processes already running
         sem.P(EXE);
 
         // Accessing critical area (shared memory)
         sem.P(BUF); 
         index = getModNum(shmbuf);
         sem.V(BUF);
+
+        srand((unsigned)time(0));
+
+        // Determines the mod number. Index = 0, use u (827395609), else use v (962094883).
         modNum = index == 0 ? 827395609 : 962094883;
-// Get rid
-        if(resume) {
-            cout << "NEW CHILD: " << getpid() << " running, using: " << modNum << endl;
-            resume = false;
-        }
-//
         
         // Generates random numbers until number is less than 100 or divisible by its modNum
         while(true) {
             int randNum = rand(); // Generate random numbers
 
-            // Test if number less than 100 or divisible by X
+            // Test if number less than 100 or divisible by modNum
             if(randNum < 100 || randNum % modNum == 0) {
-                // Breaks out of loop (no longer running)
-                cout << getpid() << " Leaving: " << randNum << " index: " << index<< endl;
-                
+                // If it is, queue itself and allow next child to execute
+    
                 // Update flag in critical area to allow next process to use this modNum
                 sem.P(BUF);
                 *(shmbuf + index) = '1';
                 sem.V(BUF); 
 
-                sem.V(EXE); // Resumes next process in queue
-// Get rid
-                if(!resume) {
-                    resume = true;
-                }
-//
+                // Resumes next process in queue
+                sem.V(EXE); 
                 break;
             }
         }
     }
 }
+
 /**
-* Cleans up the program by killing all 4 children process 
-* and removing both the shared memory and the semaphores.
-* 
+Cleans up the program by killing all 4 children process 
+and removing both the shared memory and the semaphore.
+    @param sem - The semaphore object.
+    @param children - The process IDs of all four children.
+    @param shmid - The ID of the shared memory. Used for deallocating shared memory. 
 */
 void quit(SEMAPHORE & sem, pid_t children[], int shmid) {
-    // Goes through all children process IDs and kills them.
+    // Goes through each child's process ID and kills it.
     for(int i = 0; i < sizeof(* children); i++) {
-        cout << "Killing: " << children[i] << endl;
         kill(children[i], SIGTERM);
     }
     // Cleans up shared memory
     shmctl(shmid, IPC_RMID, NULL);
-    cout << "Killed all children, parent leaving" << endl;
 
     // Removes semaphore
     sem.remove();
